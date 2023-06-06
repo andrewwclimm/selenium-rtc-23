8375771b976126d334fefe8918d584a217eae8cf
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 09:58:59 +0300

    add browser driver

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
 public class FirstTest extends BrowserGetter{
+    WebDriver driver;
+    BrowserGetter browserGetter = new BrowserGetter();
+    @BeforeAll
+    void beforeAll() {
+        driver = browserGetter.getChrome();
+    }
 }
