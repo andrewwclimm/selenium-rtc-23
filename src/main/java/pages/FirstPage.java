672951030ed15520ee680f3527f1318f56ae0543
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 12:08:56 +0300

    remove dead code

--- src/main/java/pages/FirstPage.java
+++ src/main/java/pages/FirstPage.java
@@ -1,13 +1,10 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class FirstPage {
 
-//    @FindBy(id = "idModuleImage") public WebElement elementWithId;
-//    @FindBy(css ="#idModuleImage") public WebElement elementWithCSS;
     @FindBy(className = "w3-btn") public WebElement elementWithClass;
     @FindBy(css = ".w3-btn.w3-padding.w3-border.w3-sand") public WebElement elementWithManyClasses;
     @FindBy(tagName = "h2") public WebElement elementWithTagName;
