12825392d05316b9f4ad5c4da265af1db3371550
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 14:36:57 +0300

    h5 element

--- src/main/java/pages/FirstPage.java
+++ src/main/java/pages/FirstPage.java
@@ -5,15 +5,17 @@ import org.openqa.selenium.support.FindBy;
 
 public class FirstPage {
 
-    @FindBy(className = "w3-btn") public WebElement elementWithClass;
-    @FindBy(css = ".w3-btn.w3-padding.w3-border.w3-sand") public WebElement elementWithManyClasses;
-    @FindBy(tagName = "h2") public WebElement elementWithTagName;
-    @FindBy(css = "[width='189']") public WebElement imageWithWidth;
-    @FindBy(css = "[src*='1878']") public WebElement imageWithSrc;
+    @FindBy(className="w3-btn") public WebElement elementWithClass;
+    @FindBy(css=".w3-btn.w3-padding.w3-border.w3-sand") public WebElement elementWithManyClasses;
+    @FindBy(tagName="h2") public WebElement elementWithTagName;
+    @FindBy(css="[width='189']") public WebElement imageWithWidth;
+    @FindBy(css="[src*='1878']") public WebElement imageWithSrc;
 
-    @FindBy(css = "[type=\"checkbox\"]") public WebElement checkbox;
-    @FindBy(css = "[type=\"radio\"]") public WebElement radio;
+    @FindBy(css="[type=\"checkbox\"]") public WebElement checkbox;
+    @FindBy(css="[type=\"radio\"]") public WebElement radio;
 
-    @FindBy(css= "[name = 'coffee']") public WebElement coffee;
+    @FindBy(css="[name='coffee']") public WebElement coffee;
+
+    @FindBy(css="h5") public WebElement h5Element;
 
 }
