a6c18f15014455cd26f8070d4f91e690716ed253
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 09:50:59 +0300

    add remote allow origins

--- src/main/java/BrowserGetter.java
+++ src/main/java/BrowserGetter.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserGetter {
     public WebDriver getChrome() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         return driver;
     }
 }
