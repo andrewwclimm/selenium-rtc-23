ab5d74c645a68a9ecdef8c870d72ba1605bac3a0
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 10:03:10 +0300

    add test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,13 +1,20 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTest extends BrowserGetter{
     WebDriver driver;
     BrowserGetter browserGetter = new BrowserGetter();
     @BeforeAll
     void beforeAll() {
         driver = browserGetter.getChrome();
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("some");
     }
 
     @AfterAll
