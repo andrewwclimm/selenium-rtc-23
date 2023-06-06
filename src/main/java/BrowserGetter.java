ef74ac1beaa185edd061dc844ca6adb14143d626
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 16:48:39 +0300

    remove dead code

--- src/main/java/BrowserGetter.java
+++ src/main/java/BrowserGetter.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserGetter {
     public WebDriver getChrome() {
