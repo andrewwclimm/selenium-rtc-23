0463cefdb22987f3b3a7d0b76676efe3e91b5082
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 09:56:05 +0300

    add firefox driver

--- src/main/java/BrowserGetter.java
+++ src/main/java/BrowserGetter.java
@@ -1,12 +1,22 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserGetter {
     public WebDriver getChrome() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public WebDriver getFirefox() {
+//        FirefoxOptions firefoxOptions = new FirefoxOptions();
+//        firefoxOptions.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         return driver;
     }
