1e4bc8b640889650d7860b4493ef38dea7312110
Andrei Clim <andrei.clim@RO-Y93CWQ91J7>
2023-06-06 09:41:52 +0300

    webdriver class

---
+++ src/main/java/BrowserGetter.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserGetter {
+    public WebDriver getChrome() {
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
+}
